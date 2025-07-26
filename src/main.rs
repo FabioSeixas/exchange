0b3b429ece930530b4f943e9f78de8a31653dcc3
fabioseixas <dev.fabioseixas@gmail.com>
2025-07-26 05:27:27 -0300

    feat: implement add ask on book

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::collections::VecDeque;
+use std::collections::{LinkedList, VecDeque};
 
+// #[derive(Debug, Clone, Copy)]
 #[derive(Debug)]
 struct Order {
     pub id: u16,
@@ -16,6 +17,12 @@ impl Queue {
     fn new() -> Self {
         Self {
             items: VecDeque::new(),
+        }
+    }
+
+    fn new_with_order(order: Order) -> Self {
+        Self {
+            items: VecDeque::from([order]),
         }
     }
 
@@ -77,26 +84,76 @@ impl Queue {
     }
 }
 
+#[derive(Debug)]
 struct LinkedItem {
-    next: Box<LinkedItem>,
-    queue: Box<LinkedItem>,
+    price: u16,
+    queue: Queue,
 }
 
+impl LinkedItem {
+    fn new(order: Order) -> Self {
+        Self {
+            price: order.price,
+            queue: Queue::new_with_order(order),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Book {
-    bid: LinkedItem,
-    ask: LinkedItem,
+    highest_bid: u16,
+    highest_ask: u16,
+    bid: LinkedList<LinkedItem>,
+    ask: LinkedList<LinkedItem>,
 }
-
-// struct Ask { }
-//
-// impl Order for Ask {
-//
-// }
 
 impl Book {
-    fn addAsk(order: Order) {}
+    fn new() -> Self {
+        Self {
+            highest_bid: 0,
+            highest_ask: 0,
+            bid: LinkedList::new(),
+            ask: LinkedList::new(),
+        }
+    }
 
-    fn addBid(order: Order) {}
+    fn add_ask(&mut self, order: Order) {
+        if let Some(highest_ask_item) = self.ask.front_mut() {
+            match self.highest_ask.cmp(&order.price) {
+                std::cmp::Ordering::Less => {
+                    self.highest_ask = order.price;
+                    self.ask.push_front(LinkedItem::new(order));
+                    return;
+                }
+                std::cmp::Ordering::Equal => {
+                    highest_ask_item.queue.add(order);
+                    return;
+                }
+                std::cmp::Ordering::Greater => {
+                    // nothing
+                }
+            };
+        }
+
+        dbg!(&order);
+
+        for price_level in self.ask.iter_mut() {
+            if price_level.price == order.price {
+                price_level.queue.add(order);
+                return;
+            } else {
+                // go to a lower price level
+            }
+        }
+
+        if self.ask.front().is_none() {
+            self.highest_ask = order.price;
+        }
+
+        self.ask.push_back(LinkedItem::new(order));
+    }
+
+    fn add_bid(order: Order) {}
 }
 
 fn main() {
@@ -176,5 +233,120 @@ mod tests {
 
         assert_eq!(remaining_item.id, 4);
         assert_eq!(remaining_item.size, 125);
+    }
+
+    #[test]
+    fn add_first_ask() {
+        let mut book = Book::new();
+
+        let a = Order {
+            id: 1,
+            size: 100,
+            price: 100,
+        };
+        let b = Order {
+            id: 2,
+            size: 200,
+            price: 100,
+        };
+
+        book.add_ask(a);
+        book.add_ask(b);
+
+        assert_eq!(book.highest_ask, 100);
+        assert_eq!(book.ask.len(), 1);
+
+        let price_level = book.ask.front().unwrap();
+        assert_eq!(price_level.queue.items.len(), 2);
+        assert_eq!(
+            price_level.queue.items.iter().map(|i| i.size).sum::<u16>(),
+            300
+        );
+    }
+
+    #[test]
+    fn higher_ask_price_level() {
+        let mut book = Book {
+            highest_ask: 50,
+            highest_bid: 0,
+            bid: LinkedList::new(),
+            ask: LinkedList::from([LinkedItem {
+                price: 50,
+                queue: Queue {
+                    items: VecDeque::from([Order {
+                        price: 50,
+                        size: 100,
+                        id: 1,
+                    }]),
+                },
+            }]),
+        };
+
+        let a = Order {
+            id: 2,
+            size: 100,
+            price: 100,
+        };
+        let b = Order {
+            id: 3,
+            size: 200,
+            price: 100,
+        };
+
+        book.add_ask(a);
+        book.add_ask(b);
+
+        assert_eq!(book.highest_ask, 100);
+        assert_eq!(book.ask.len(), 2);
+
+        let highest_price_level = book.ask.front().unwrap();
+        assert_eq!(highest_price_level.queue.items.len(), 2);
+        assert_eq!(
+            highest_price_level.queue.items.iter().map(|i| i.size).sum::<u16>(),
+            300
+        );
+    }
+
+    #[test]
+    fn lower_ask_price_level() {
+        let mut book = Book {
+            highest_ask: 500,
+            highest_bid: 0,
+            bid: LinkedList::new(),
+            ask: LinkedList::from([LinkedItem {
+                price: 500,
+                queue: Queue {
+                    items: VecDeque::from([Order {
+                        price: 500,
+                        size: 100,
+                        id: 1,
+                    }]),
+                },
+            }]),
+        };
+
+        let a = Order {
+            id: 2,
+            size: 100,
+            price: 100,
+        };
+        let b = Order {
+            id: 3,
+            size: 200,
+            price: 100,
+        };
+
+        book.add_ask(a);
+        book.add_ask(b);
+
+        assert_eq!(book.highest_ask, 500);
+        assert_eq!(book.ask.len(), 2);
+
+        let lowest_price_level = book.ask.back().unwrap();
+        assert_eq!(lowest_price_level.queue.items.len(), 2);
+        assert_eq!(
+            lowest_price_level.queue.items.iter().map(|i| i.size).sum::<u16>(),
+            300
+        );
     }
 }
